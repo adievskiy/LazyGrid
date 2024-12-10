a3a911e2d6a69258d1a9a9aaf89623cbd590346e
adievskiy <dievskiy@mail.ru>
2024-12-10 13:55:27 +0400

    complete

--- app/src/main/java/com/example/lazygrid/MainActivity.kt
+++ app/src/main/java/com/example/lazygrid/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -15,9 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.lazygrid.ui.theme.LazyGridTheme
 
 class MainActivity : ComponentActivity() {
 
